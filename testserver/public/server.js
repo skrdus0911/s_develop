0cd3adea0a67e48ba9fa01b048c701c0c7957387
skrdus0911 <hgu21200761@gmail.com>
2018-07-04 15:14:40 +0900

    TCP server test

---
+++ testserver/public/server.js
@@ -0,0 +1,71 @@
+const net = require('net');
+const fs = require('fs');
+const path = require('path');
+
+console.log('server start');
+
+const checkContentType = (pathname) => {
+  const type = path.parse(pathname);
+  const contentType = {
+    '.css': 'text/css',
+    '.html': 'text/html',
+  }
+  return contentType[type.ext];
+}
+
+const httpHeader = (url, body) => {
+  return [
+    'HTTP/1.1 200 OK',
+    'Content-Type: ' + checkContentType(url),
+    'Content-Length: ' + body.length
+  ].join('\r\n') + '\r\n\r\n'
+}
+
+const server = net.createServer((socket) => {
+
+  socket.on('data', (data) => {
+    const req = data.toString();
+    const lines = req.split('\r\n');
+    const [method, url, ver] = lines[0].split(" ");
+  
+    let pathname = url;
+
+    if(url ==='/'){
+      pathname = '/index.html';
+    }
+
+    fs.readFile(__dirname+pathname, (err, filedata) => {
+      if (err) {
+        console.log(err);
+        socket.write("HTTP/1.1 404");
+        socket.write("Content-Type: text/html");
+        socket.write("\r\n\r\n");
+        socket.write("404 not found")
+      } else {
+        socket.write(httpHeader(url, filedata));
+        socket.write(filedata.toString());
+      }
+      socket.end();
+    });
+
+  });
+
+});
+
+server.on('listening', () => {
+  console.log('onListening');
+});
+
+server.on('error', (err) => {
+  throw err;
+});
+
+server.on('close', () => {
+  console.log('client disconnected');
+});
+
+server.listen(3000, '127.0.0.1');
+
+console.log('server close');
+
+
